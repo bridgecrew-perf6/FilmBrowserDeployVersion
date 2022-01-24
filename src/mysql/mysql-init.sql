c0dc57643957dbc21a40d061a58c826d2540d5f8
Kuba <jakubzengota@gmail.com>
2022-01-24 11:38:36 +0100

    adding all files

---
+++ src/mysql/mysql-init.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE myfilms;
+USE myfilms;
+
+CREATE TABLE `myfilms`.`films` (
+`id` INT NOT NULL AUTO_INCREMENT,
+`name` TEXT(255) NOT NULL,
+`type` TEXT(255) NOT NULL,
+`year` INT NOT NULL,
+`country` TEXT(255) NOT NULL,
+PRIMARY KEY (`id`)
+);
